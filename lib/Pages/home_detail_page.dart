528296bfa7b164a558736219286515fab2a0221c
raman020306 <ramankumar060203@gmail.com>
2024-05-26 15:13:27 +0530

    Day18 completed

--- lib/Pages/home_detail_page.dart
+++ lib/Pages/home_detail_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter_application_1/models/catalogApp.dart';
 import 'package:flutter_application_1/utills/widgets/themes.dart';
 import 'package:velocity_x/velocity_x.dart';
 
-
 class HomeDetailPage extends StatelessWidget {
   final Item catalog;
 
@@ -13,28 +12,30 @@ class HomeDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
       backgroundColor: MyTheme.creamColor,
       bottomNavigationBar: Container(
         color: Colors.white,
         child: ButtonBar(
-                  alignment: MainAxisAlignment.spaceBetween,
-                  buttonPadding: EdgeInsets.zero,
-                  children: [
-                    "\$${catalog.price}".text.bold.xl4.red800.make(),
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(
-                            MyTheme.lightGreenAccent,
-                          ),
-                          shape: MaterialStateProperty.all(
-                            StadiumBorder(),
-                          )),
-                      child: "Buy".text.make(),
-                    ).wh(100, 50)
-                  ],
-                ).p32(),
+          alignment: MainAxisAlignment.spaceBetween,
+          buttonPadding: EdgeInsets.zero,
+          children: [
+            "\$${catalog.price}".text.bold.xl4.red800.make(),
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                    MyTheme.lightGreenAccent,
+                  ),
+                  shape: MaterialStateProperty.all(
+                    StadiumBorder(),
+                  )),
+              child: "Buy".text.make(),
+            ).wh(100, 50)
+          ],
+        ).p32(),
       ),
       body: SafeArea(
         bottom: false,
@@ -42,27 +43,32 @@ class HomeDetailPage extends StatelessWidget {
           children: [
             Hero(
               tag: Key(catalog.id.toString()),
-              child: Image.network(
-                catalog.image),
+              child: Image.network(catalog.image),
             ).h32(context),
-            Expanded(child: VxArc(
-              
+            Expanded(
+                child: VxArc(
               height: 30.0,
               arcType: VxArcType.CONVEY,
               edge: VxEdge.TOP,
               child: Container(
                 color: Color.fromARGB(255, 225, 218, 218),
                 width: context.screenWidth,
-                child: Column(children: [
-                   catalog.name.text.xl4
-                   . color(MyTheme.lightGreenAccent).bold.make(),
-              catalog.desc.text.textStyle(context.captionStyle).xl.make(),
-              10.heightBox,
-                ],).py64(),
+                child: Column(
+                  children: [
+                    catalog.name.text.xl4
+                        .color(MyTheme.lightGreenAccent)
+                        .bold
+                        .make(),
+                    catalog.desc.text.textStyle(context.captionStyle).xl.make(),
+                    10.heightBox,
+                    "Dummy Text: Computers, mobile phones, and calculators are all electronic devices that process and manipulate information, but they differ in terms of their capabilities, functionality, and purpose."
+                        .text.textStyle(context.captionStyle)
+                        .make().p16(),
+                  ],
+                ).py64(),
               ),
-            )
-            )
-            ],
+            ))
+          ],
         ),
       ),
     );
